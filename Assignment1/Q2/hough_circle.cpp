5729d981c39d6847031cfe9587592108feceed30
Vignesh Iyer <viiy1398@colorado.edu>
2019-06-13 13:20:36 -0600

    Assignment1

---
+++ Assignment1/Q2/hough_circle.cpp
@@ -0,0 +1,56 @@
+
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/highgui.hpp"
+#include "opencv2/imgproc.hpp"
+#include <stdio.h>
+#include <vector>
+#include <thread>
+#include <opencv2/opencv.hpp>
+#include "opencv2/core/core.hpp"
+#include "opencv2/features2d/features2d.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/calib3d/calib3d.hpp"
+//#include "opencv2/nonfree/nonfree.hpp"
+#include <iostream>
+using namespace cv;
+
+/** @function main */
+int main(int argc, char** argv)
+{
+  Mat src, src_gray;
+
+  /// Read the image
+  src = imread( argv[1], 1 );
+
+  if( !src.data )
+    { return -1; }
+
+  /// Convert it to gray
+  cvtColor( src, src_gray, CV_BGR2GRAY );
+
+  /// Reduce the noise so we avoid false circle detection
+  GaussianBlur( src_gray, src_gray, Size(9, 9), 2, 2 );
+
+  std::vector<Vec3f> circles;
+
+  /// Apply the Hough Transform to find the circles
+  HoughCircles( src_gray, circles, CV_HOUGH_GRADIENT, 1, src_gray.rows/16, 60, 60, 0, 0 );
+
+  /// Draw the circles detected
+  for( size_t i = 0; i < circles.size(); i++ )
+  {
+      Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+      int radius = cvRound(circles[i][2]);
+      // circle center
+      circle( src, center, 3, Scalar(0,255,0), -1, 8, 0 );
+      // circle outline
+      circle( src, center, radius, Scalar(0,0,255), 3, 8, 0 );
+   }
+
+  /// Show your results
+  namedWindow( "Hough Circle Transform Demo", CV_WINDOW_AUTOSIZE );
+  imshow( "Hough Circle Transform Demo", src );
+
+  waitKey(0);
+  return 0;
+}
---
+++ Assignment1/q5/captureimg.cpp
@@ -0,0 +1,122 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <iostream>
+
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+#include "opencv2/calib3d/calib3d.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+//#include "opencv2/contrib/contrib.hpp"
+
+using namespace cv;
+using namespace std;
+
+// Default resolution is 180p
+#define VRES_ROWS (180)
+#define HRES_COLS (240)
+
+#define ESC_KEY (27)
+
+// Buffer for highest resolution visualization possible
+unsigned char imagebuffer[1440*2560*3]; // 1440 rows, 2560 cols/row, 3 channel
+
+int main(int argc, char **argv)
+{
+    int hres = HRES_COLS;
+    int vres = VRES_ROWS;
+    Size size(180,240);
+    Mat basicimage(vres, hres, CV_8UC3, imagebuffer);
+    Mat editImage(vres, hres, CV_8UC3, imagebuffer);
+    Mat src1(vres, hres, CV_8UC3, imagebuffer);
+
+    Scalar value;
+    int frameCnt=0;
+    int up,down,side_left,side_right;
+
+    printf("hres=%d, vres=%d\n", hres, vres);
+
+    // interactive computer vision loop 
+    namedWindow("Profile Visualization", CV_WINDOW_AUTOSIZE);
+
+    // read in default image
+    if(vres == 180)
+	basicimage = imread("Cows.jpg", CV_LOAD_IMAGE_COLOR);
+    else if(vres == 360)
+        basicimage = imread("Cows.jpg", CV_LOAD_IMAGE_COLOR);
+    else if(vres == 720)
+        basicimage = imread("Cows.jpg", CV_LOAD_IMAGE_COLOR);
+    else if(vres == 1080)
+        basicimage = imread("Cows.jpg", CV_LOAD_IMAGE_COLOR);
+    else if(vres == 1440)
+        basicimage = imread("Cows.jpg", CV_LOAD_IMAGE_COLOR);
+
+    if(!basicimage.data)  // Check for invalid input
+    {
+        printf("Could not open or find the refernece starting image\n");
+        exit(-1);
+    }
+	cv::resize(basicimage,basicimage,size,0,0,INTER_LINEAR);	
+	up= 4;
+	down=4;
+	side_left=4;
+	side_right=4;
+	editImage=basicimage;
+	src1=editImage;
+	value = Scalar(0,0,255);
+	copyMakeBorder( basicimage, editImage, up, down, side_left, side_right, BORDER_CONSTANT, value );
+	//Assinging intensity of BGR to 3 variables	
+	/*Vec3b changeInt=editImage.at<Vec3b>(92,162);
+	uchar blue = changeInt.val[0];
+	uchar green = changeInt.val[1];
+	uchar red = changeInt.val[2];
+	*/
+	for(int i=82;i<=102;i++)
+	{
+		editImage.at<Vec3b>(122,i)[0]=0;
+		editImage.at<Vec3b>(122,i)[1]=255;
+		editImage.at<Vec3b>(122,i)[2]=255;
+	}	
+	for(int j=112;j<=132;j++)
+	{	
+		editImage.at<Vec3b>(j,92)[0]=0;
+		editImage.at<Vec3b>(j,92)[1]=255;
+		editImage.at<Vec3b>(j,92)[2]=255;
+		
+	}	
+	
+	cout << "Width : " << basicimage.cols << endl;
+	cout << "Height: " << basicimage.rows << endl;
+	// Create an IplImage mapping onto the basicimage Mat object
+    //
+    IplImage basicimageImg = basicimage;
+
+
+    // Interactive LOOP
+    //
+    while(1)
+    {
+        frameCnt++;
+	
+        // Write a zero value into the image buffer
+        //
+        basicimageImg.imageData[frameCnt] = (unsigned char)0;
+	 imshow("Original", basicimage);
+        imshow("Edit", editImage);  
+	// imshow("Edit1", src1);  
+
+        // set pace on shared memory sampling in msecs
+        char c = cvWaitKey(10);
+
+        if(c == ESC_KEY)
+        {
+            exit(1);
+        }
+
+    }
+ 
+    return 1;
+
+}
