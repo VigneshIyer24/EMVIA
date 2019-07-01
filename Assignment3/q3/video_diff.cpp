927d54c7d5c768128c73a5e9809a71ae2b29e3b3
Vignesh Iyer <43046138+VigneshIyer24@users.noreply.github.com>
2019-07-01 17:16:42 -0600

    Add files via upload

--- Assignment3/q3/video_diff.cpp
+++ Assignment3/q3/video_diff.cpp
@@ -1,10 +1,15 @@
-/*
+/**
+ *@file: video_diff.cpp
+ *   
+ *@description: cpp code that performs background subtaction using
+ *  			frame differencing in the G-band
  *
- *  Example by Sam Siewert 
+ *@Author: Code written by Vignesh Iyer based on code written by Prof. Sam Siewert
  *
- *  Updated 12/6/18 for OpenCV 3.1
- *
+ *@Date: 07/01/2019
  */
+
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
@@ -27,8 +32,9 @@ int main( int argc, char** argv )
     double percent_diff;
 
 
-    //open the video stream and make sure it's opened
-    // "0" is the default video device which is normally the built-in webcam
+    /*open the video stream and make sure it's opened*/
+
+    /* "0" is the default video device which is normally the built-in webcam*/
     if(!vcap.isOpened()) 
     {
         std::cout << "Error opening video stream or file" << std::endl;
@@ -45,11 +51,14 @@ int main( int argc, char** argv )
     }
 	
     cv::cvtColor(mat_frame, mat_gray, CV_BGR2GRAY);
-
+    /*
+    *Code for creating a clone. mat_diff stores the clone.
+    *mat_gray_prev stores the grayscvale version of the mat_gray
+    */
     mat_diff = mat_gray.clone();
     mat_gray_prev = mat_gray.clone();
 
-    maxdiff = (mat_diff.cols)*(mat_diff.rows)*255;
+    maxdiff = (mat_diff.cols)*(mat_diff.rows)*255;/*Calcualates the number pixels*/
 
     while(1)
     {
@@ -59,12 +68,12 @@ int main( int argc, char** argv )
 		
 	}
 	
-	cv::cvtColor(mat_frame, mat_gray, CV_BGR2GRAY);
+	cv::cvtColor(mat_frame, mat_gray, CV_BGR2GRAY);/*Converts from RGB to gray*/
 
-	absdiff(mat_gray_prev, mat_gray, mat_diff);
+	absdiff(mat_gray_prev, mat_gray, mat_diff);/*stores the difference between mat_gray_prev, mat_gray*/
 
-	// worst case sum is resolution * 255
-	diffsum = (unsigned int)cv::sum(mat_diff)[0]; // single channel sum
+	/* worst case sum is resolution * 255*/
+	diffsum = (unsigned int)cv::sum(mat_diff)[0]; /*single channel sum*/
 
 	percent_diff = ((double)diffsum / (double)maxdiff)*100.0;
 
@@ -73,7 +82,9 @@ int main( int argc, char** argv )
 
         // tested in ERAU Jetson lab
 	if(percent_diff > 0.5) cv::putText(mat_diff, difftext, cvPoint(30,30), FONT_HERSHEY_COMPLEX_SMALL, 0.8, cvScalar(200,200,250), 1, CV_AA);
-
+	/*
+	*Displays the current frame
+	*/
 	cv::imshow("Gray Example", mat_gray);
 	cv::imshow("Gray Previous", mat_gray_prev);
 	cv::imshow("Gray Diff", mat_diff);
