d87fcaa7bf71dbf34d369c291de1647bbbdc4d16
Vignesh Iyer <43046138+VigneshIyer24@users.noreply.github.com>
2019-07-01 17:17:27 -0600

    Add files via upload

--- Assignment3/q2/median_filter.cpp
+++ Assignment3/q2/median_filter.cpp
@@ -1,18 +1,20 @@
-/*----------------------
- * Vignesh Iyer
- * Created on 06/24/2019
- *-----------------------*/
+/**
+ *@file: median_filter.cpp
+ *   
+ *@description: uses median filtering to enhance edges in the G band.
+ *
+ *@Author: Code written by Vignesh Iyer based on code written by Prof. Sam Siewert
+ *
+ *@Date: 07/01/2019
+ */
+
+
 #include <iostream>
 
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
 
-/*----------------------------
- * Kernel length for median
- * filter which is the window
- * size
- *----------------------------*/
 #define MAX_KERNEL_LENGTH 31
 
 using namespace cv;
@@ -24,35 +26,33 @@ Mat bgr_img[3];
 Mat G_img;
 Mat gry_img;
 Mat mod_img;
-
 char window_name[]= "Median Filter G band";
 
 int main(int argc, char **argv)
 {
 	namedWindow(window_name,WINDOW_AUTOSIZE);
-	
-	// Define the image for conversion and applying median filter
-	
+
 	const char* filename =argc >=2 ? argv[1] : "image45.ppm";
 	org_img=imread(filename,CV_LOAD_IMAGE_COLOR);
 	namedWindow( "Original Image", CV_WINDOW_AUTOSIZE );
  	imshow( "Original Image", org_img );
-	/*----------------------------------------
-	 * Split used to get all the three B, 
-	 * G , R components.The output is always
-	 * a grayscale image
-	 ----------------------------------------*/
+	/*This API splits an image into the BGR components that comprise it.
+	*Here bgr_img is an array of size 3 which stores the BGR values respectively
+	*/
 	split(org_img,bgr_img);
 
-	G_img=bgr_img[3];
+	G_img=bgr_img[1];
 
-	imwrite("green_img.ppm",G_img);	
+	imwrite("green_img.jpg",bgr_img[1]);	
 	if(org_img.empty())
 	{
 		printf("Error opening image\n");
 		return -1;
 	}
-	// Median filter
+	/*
+	*Median filter works by taking a pixel and differentiating it with neighbouring pixels
+	*according to the size of the window of pixels. 
+	*/
 	for ( int i = 1; i < MAX_KERNEL_LENGTH; i = i + 2 )
     	{
         	medianBlur ( bgr_img[1], mod_img, i );
